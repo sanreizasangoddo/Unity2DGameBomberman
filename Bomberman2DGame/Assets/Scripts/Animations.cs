c797dfe8ba21a48390d174b7017b5a1e6bed26e0
Y.Ouallal <2235702@talnet.nl>
2026-03-31 09:26:27 +0200

    Player Animations Added
    I will be continuing with it later as it's not the main priority. I also added the Player 2 sprite.

---
+++ Bomberman2DGame/Assets/Scripts/Animations.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+
+    [SerializeField] private Sprite _idleSprite;
+    [SerializeField] private Sprite[] _animationSprites;
+
+    [SerializeField] private float _animationTime = 0.25f;
+    private int _animationFrame;
+
+    [SerializeField] private bool _loop = true;
+    [SerializeField] private bool _idle = true;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        _spriteRenderer.enabled = true;
+    }
+
+    private void OnDisable()
+    {
+        _spriteRenderer.enabled = false;
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(NextFrame), _animationTime, _animationTime);
+    }
+
+    private void NextFrame()
+    {
+        _animationFrame++;
+
+        if(_loop && _animationFrame >= _animationSprites.Length)
+        {
+            _animationFrame = 0;
+        }
+
+        if (_idle)
+        {
+            _spriteRenderer.sprite = _idleSprite;
+        }
+    }
+}
